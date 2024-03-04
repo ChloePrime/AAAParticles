5021eb210cca5078606fdecc400b5fb997d8157b
ChloePrime <chloe_von_einzbern@qq.com>
2024-03-04 16:35:07 +0800

    清理Forge侧的旧版遗留代码

--- forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForge.java
+++ forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForge.java
@@ -3,9 +3,6 @@ package mod.chloeprime.aaaparticles.forge;
 import dev.architectury.platform.forge.EventBuses;
 import mod.chloeprime.aaaparticles.AAAParticles;
 import mod.chloeprime.aaaparticles.client.AAAParticlesClient;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.forgespi.Environment;
@@ -14,16 +11,12 @@ import net.minecraftforge.forgespi.Environment;
 public class AAAParticlesForge {
     public AAAParticlesForge() {
         var modbus = FMLJavaModLoadingContext.get().getModEventBus();
-        var forgebus = MinecraftForge.EVENT_BUS;
         EventBuses.registerModEventBus(AAAParticles.MOD_ID, modbus);
         AAAParticles.init();
 
         if (Environment.get().getDist().isClient()) {
             AAAParticlesClient.init();
             modbus.addListener(AAAParticlesForgeClient::onClientSetup);
-            if (AAAParticlesForgeClient.USE_RENDER_EVENT) {
-                forgebus.addListener(AAAParticlesForgeClient::onRenderStage);
-            }
         }
     }
 }
--- forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForgeClient.java
+++ forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForgeClient.java
@@ -1,21 +1,10 @@
 package mod.chloeprime.aaaparticles.forge;
 
 import mod.chloeprime.aaaparticles.client.AAAParticlesClient;
-import mod.chloeprime.aaaparticles.client.render.EffekRenderer;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class AAAParticlesForgeClient {
-    public static final boolean USE_RENDER_EVENT = false;
-    public static final RenderLevelStageEvent.Stage RENDER_STAGE = RenderLevelStageEvent.Stage.AFTER_LEVEL;
-
-    static void onRenderStage(RenderLevelStageEvent e) {
-        if (e.getStage() != RENDER_STAGE) {
-            return;
-        }
-        EffekRenderer.onRenderWorldLast(e.getPartialTick(), e.getPoseStack(), e.getProjectionMatrix(), e.getCamera());
-    }
-
+    @SuppressWarnings("unused")
     static void onClientSetup(FMLClientSetupEvent e) {
         AAAParticlesClient.setup();
     }
