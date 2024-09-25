5408a8dc0e651365a67a4bcf1f6fc55092f2423f
ChloePrime <chloe_von_einzbern@qq.com>
2024-09-25 12:19:15 +0800

    清理imports

--- common/src/main/java/mod/chloeprime/aaaparticles/client/installer/JarExtractor.java
+++ common/src/main/java/mod/chloeprime/aaaparticles/client/installer/JarExtractor.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Objects;
