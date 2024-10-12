98f91740cf7448f1e6b74ed97da089bfc969daab
ChloePrime <chloe_von_einzbern@qq.com>
2024-10-12 17:22:53 +0800

    移除配置文件和ForgeConfigAPIPort的依赖

--- forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForgeClient.java
+++ forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForgeClient.java
@@ -2,14 +2,10 @@ package mod.chloeprime.aaaparticles.forge;
 
 import mod.chloeprime.aaaparticles.AAAParticles;
 import mod.chloeprime.aaaparticles.client.AAAParticlesClient;
-import mod.chloeprime.aaaparticles.client.ModClientConfig;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class AAAParticlesForgeClient extends AAAParticles {
     public static void onClientInit() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModClientConfig.SPEC);
     }
 
     @SuppressWarnings("unused")
