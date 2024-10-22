378a869cc605c04b44d85b2ee385592dfd2ec891
ChloePrime <chloe_von_einzbern@qq.com>
2024-10-22 20:41:35 +0800

    修复Datagen检测

--- forge/src/main/java/mod/chloeprime/aaaparticles/client/installer/forge/NativePlatformImpl.java
+++ forge/src/main/java/mod/chloeprime/aaaparticles/client/installer/forge/NativePlatformImpl.java
@@ -1,8 +1,21 @@
 package mod.chloeprime.aaaparticles.client.installer.forge;
 
+import com.google.common.base.Suppliers;
+import net.neoforged.fml.loading.FMLLoader;
+
+import java.util.function.Supplier;
+
 @SuppressWarnings("unused")
 public class NativePlatformImpl {
+    private static final Supplier<Boolean> IS_DATA = Suppliers.memoize(
+            () -> {
+                var name = FMLLoader.launcherHandlerName();
+                return "forgedata".equals(name) ||
+                        "forgedatadev".equals(name) ||
+                        "forgedatauserdev".equals(name);
+            });
+
     public static boolean isDataGen() {
-        return false;
+        return IS_DATA.get();
     }
 }
