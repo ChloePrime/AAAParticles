2f78f7a0dbd4390852b17934689a2e9c8d9d884e
Chloe Prime <chloe_von_einzbern@qq.com>
2026-04-10 22:15:13 +0800

    优化矩阵转置：移除中转 Buffer
    (cherry picked from commit b7ceb5a98dc0d94c25d402a21cbd8d3e76b409a5)

--- common/src/main/java/mod/chloeprime/aaaparticles/client/render/EffekRenderer.java
+++ common/src/main/java/mod/chloeprime/aaaparticles/client/render/EffekRenderer.java
@@ -20,9 +20,7 @@ import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.InteractionHand;
 import org.joml.Matrix4f;
-import org.lwjgl.BufferUtils;
 
-import java.nio.FloatBuffer;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -34,8 +32,6 @@ import static org.lwjgl.opengl.GL11.*;
  * @author ChloePrime
  */
 public class EffekRenderer {
-    private static final FloatBuffer CAMERA_TRANSFORM_BUFFER = BufferUtils.createFloatBuffer(16);
-    private static final FloatBuffer PROJECTION_BUFFER = BufferUtils.createFloatBuffer(16);
     private static final AtomicBoolean INIT = new AtomicBoolean();
 
     public static void init() {
@@ -101,9 +97,8 @@ public class EffekRenderer {
         int w = MINECRAFT.getWindow().getWidth();
         int h = MINECRAFT.getWindow().getHeight();
 
-        projection.get(PROJECTION_BUFFER);
-        transposeMatrix(PROJECTION_BUFFER);
-        PROJECTION_BUFFER.get(PROJECTION_MATRIX_DATA);
+        projection.get(PROJECTION_MATRIX_DATA);
+        transposeMatrix(PROJECTION_MATRIX_DATA);
 
         pose.pushPose();
         {
@@ -111,9 +106,8 @@ public class EffekRenderer {
                 pose.translate(-camera.getPosition().x(), -camera.getPosition().y(), -camera.getPosition().z());
             }
 
-            pose.last().pose().get(CAMERA_TRANSFORM_BUFFER);
-            transposeMatrix(CAMERA_TRANSFORM_BUFFER);
-            CAMERA_TRANSFORM_BUFFER.get(CAMERA_TRANSFORM_DATA);
+            pose.last().pose().get(CAMERA_TRANSFORM_DATA);
+            transposeMatrix(CAMERA_TRANSFORM_DATA);
         }
         pose.popPose();
 
@@ -140,50 +134,10 @@ public class EffekRenderer {
         });
 
         RenderType.PARTICLES_TARGET.clearRenderState();
-
-        CAMERA_TRANSFORM_BUFFER.clear();
-        PROJECTION_BUFFER.clear();
     }
 
-    private static void transposeMatrix(FloatBuffer m) {
-        float m00, m01, m02, m03;
-        float m10, m11, m12, m13;
-        float m20, m21, m22, m23;
-        float m30, m31, m32, m33;
-
-        m00 = m.get(0);
-        m01 = m.get(1);
-        m02 = m.get(2);
-        m03 = m.get(3);
-        m10 = m.get(4);
-        m11 = m.get(5);
-        m12 = m.get(6);
-        m13 = m.get(7);
-        m20 = m.get(8);
-        m21 = m.get(9);
-        m22 = m.get(0xA);
-        m23 = m.get(0xB);
-        m30 = m.get(0xC);
-        m31 = m.get(0xD);
-        m32 = m.get(0xE);
-        m33 = m.get(0xF);
-
-        m.put(0 , m00);
-        m.put(1 , m10);
-        m.put(2 , m20);
-        m.put(3 , m30);
-        m.put(4 , m01);
-        m.put(5 , m11);
-        m.put(6 , m21);
-        m.put(7 , m31);
-        m.put(8 , m02);
-        m.put(9 , m12);
-        m.put(0xA , m22);
-        m.put(0xB , m32);
-        m.put(0xC , m03);
-        m.put(0xD , m13);
-        m.put(0xE , m23);
-        m.put(0xF , m33);
+    private static void transposeMatrix(float[] m) {
+        RenderUtil.transposeMatrix4x4(m);
     }
 
     public static final class MinecraftHolder {
