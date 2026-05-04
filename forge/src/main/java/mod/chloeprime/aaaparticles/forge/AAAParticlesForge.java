19158710d7f6a94bd5ece649dcb1f59194c6a96a
Chloe Prime <chloe_von_einzbern@qq.com>
2026-05-05 02:54:32 +0800

    [26.1] 升级 26.1.2

--- forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForge.java
+++ forge/src/main/java/mod/chloeprime/aaaparticles/forge/AAAParticlesForge.java
@@ -2,20 +2,18 @@ package mod.chloeprime.aaaparticles.forge;
 
 import mod.chloeprime.aaaparticles.AAAParticles;
 import mod.chloeprime.aaaparticles.client.AAAParticlesClient;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(AAAParticles.MOD_ID)
 public class AAAParticlesForge extends AAAParticles  {
-    public AAAParticlesForge(IEventBus modbus, ModContainer container) {
+    public AAAParticlesForge(ModContainer container) {
         AAAParticles.init();
 
         if (FMLLoader.getCurrent().getDist().isClient()) {
             AAAParticlesForgeClient.onClientInit(container);
             AAAParticlesClient.init();
-            modbus.addListener(AAAParticlesForgeClient::onClientSetup);
         }
     }
 }
